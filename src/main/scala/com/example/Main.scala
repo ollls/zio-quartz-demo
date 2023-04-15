3ed9713b9119dbccf3ca9f0ef63860d8174c4541
ostrygun <oleg_strygun@intuit.com>
2023-04-15 11:52:09 -0700

    Routes with ZIO direct

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -16,69 +16,77 @@ import zio.stream.ZStream
 import java.util.concurrent.ConcurrentHashMap
 import scala.jdk.CollectionConverters.ConcurrentMapHasAsScala
 import io.quartz.services.H2Client
+import zio.direct._
+
+case class ChatGPTMessage(role: String, content: String)
+case class ChatGPTAPIRequest(model: String, temperature: Float, messages: Array[ChatGPTMessage])
+
+given JsonValueCodec[ChatGPTAPIRequest] = JsonCodecMaker.make
 
 //To re-generate slef-signed cert use.
 //keytool -genkey -keyalg RSA -alias selfsigned -keystore keystore.jks -storepass password -validity 360 -keysize 2048
 //in your browser: https://localhost:8443  ( click vist website, you need to accept slef-cigned cert first time )
 
-//case class Device(id: Int, model: String)
-//case class User(name: String, devices: Seq[Device])
-
 object Main extends ZIOAppDefault {
+
+  val CHAT_GPT_TOKEN = "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"
 
   override val bootstrap =
     zio.Runtime.removeDefaultLoggers ++ SLF4J.slf4j ++ zio.Runtime.enableWorkStealing
-
-  val CHAT_GPT_TOKEN = "YOUR-TOKEN"
 
   val TIMEOUT_MS = 60000
 
   val ctx = QuartzH2Client.buildSSLContext("TLSv1.3", null, null, true)
 
-  case class ChatGPTMessage(role: String, content: String)
-  case class ChatGPTAPIRequest(model: String, temperature: Float, messages: Array[ChatGPTMessage])
-
-  given JsonValueCodec[ChatGPTAPIRequest] = JsonCodecMaker.make
-
   val R: HttpRouteIO[H2Client] =
     ///////////////////////////////////////
-    case req @ GET -> Root / "test" => ZIO.attempt(Response.Ok())
+    case req @ GET -> Root / "test" =>
+      defer {
+        Response.Ok()
+      }
+
     ///////////////////////////////////////
     case req @ POST -> Root / "token" =>
-      for {
-        text <- req.body.map(String(_))
-        svc  <- ZIO.service[H2Client]
-
-        conn <- svc.getConnection(req.connId)
-
-        request <- ZIO.attempt(
-          ChatGPTAPIRequest(
-            "gpt-3.5-turbo",
-            0.7,
-            messages = Array(ChatGPTMessage("user", s"translate from English to Ukranian: '$text'"))
+      defer {
+        val input      = req.body.map(String(_)).run
+        val svc        = ZIO.service[H2Client].run
+        val connection = svc.getConnection(req.connId).run
+        // Detected the use of a mutable collection inside a defer clause (called: Array).
+        // Mutable collections can cause many potential issues as a result of defer-clause
+        // rewrites so they are not allowed (Unless it is inside of a run-call).
+        val request = ZIO
+          .succeed(
+            ChatGPTAPIRequest(
+              "gpt-3.5-turbo",
+              0.7,
+              messages =
+                Array(ChatGPTMessage("user", s"translate from English to Ukranian: '$input'"))
+            )
           )
-        )
+          .run //we did run call to address the above error
 
-        response <- conn.doPost(
-          "/v1/chat/completions",
-          ZStream.fromIterable(writeToArray(request)),
-          Headers().contentType(
-            ContentType.JSON
-          ) + ("Authorization" -> s"Bearer $CHAT_GPT_TOKEN")
-        )
-        output <- response.bodyAsText
+        val response = connection
+          .doPost(
+            "/v1/chat/completions",
+            ZStream.fromIterable(writeToArray(request)),
+            Headers().contentType(
+              ContentType.JSON
+            ) + ("Authorization" -> s"Bearer $CHAT_GPT_TOKEN")
+          )
+          .run
+        val output = response.bodyAsText.run
+        Response.Ok().contentType(ContentType.JSON).asText(output)
+      }
 
-      } yield (Response.Ok().contentType(ContentType.JSON).asText(output))
+  def onDisconnect(id: Long): ZIO[H2Client, Nothing, Unit] = defer {
+    val svc = ZIO.service[H2Client].run
+    svc.close(id).run
+  }
 
-  def onDisconnect(id: Long) = (for {
-    svc <- ZIO.service[H2Client]
-    _   <- svc.close(id)
-  } yield ())
-
-  def onConnect(id: Long) = for {
-    svc <- ZIO.service[H2Client]
-    _ <- svc.open(id, "https://api.openai.com", TIMEOUT_MS, ctx = ctx, incomingWindowSize = 184590)
-  } yield ()
+  def onConnect(id: Long) = defer {
+    val svc = ZIO.service[H2Client].run
+    svc.open(id, "https://api.openai.com", TIMEOUT_MS, ctx = ctx, incomingWindowSize = 184590).run
+  }
 
   def run: Task[ExitCode] =
     for {
